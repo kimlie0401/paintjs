9cf13556e0c6e3a9a49407df57aea7e5cdf45e70
Dong kun Kim <dk@Dongs-MacBook-Air.local>
2019-08-27 00:57:56 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x,y);
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+function onMouseUp(event){
+    painting = false;
+}
+
+function onMouseLeave(event){
+    painting = false;
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", onMouseLeave);
+}
